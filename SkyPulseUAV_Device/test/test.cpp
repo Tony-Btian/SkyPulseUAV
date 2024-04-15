689f24e21050e92d60b01e2eb315c6ba3ae59e0f
engineerNg <moxiasgabi@gmail.com>
2024-04-15 14:56:18 +0100

    unit test block added

---
+++ SkyPulseUAV_Device/test/test.cpp
@@ -0,0 +1,22 @@
+#define BOOST_TEST_MODULE Tests
+#include "boost/test/unit_test.hpp"
+#include "MPU6050.h"
+#include "IIC.h"
+#include "MahonyFilter.h"
+#include "TCP.h"
+#include "BMP180.h"
+#include "STM32.h"
+#include "IRAndUS.h"
+
+// Define Pass conditions here.
+BOOST_AUTO_TEST_CASE(PassTest)
+{
+    
+}
+
+
+// Define fail conditions here.
+BOOST_AUTO_TEST_CASE(FailTest)
+{
+    
+}

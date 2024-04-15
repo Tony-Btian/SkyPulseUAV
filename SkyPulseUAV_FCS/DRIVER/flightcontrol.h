11e5f81aad977b6502d6999e8f9a54241da4f23b
Tony-Btian <tianbinhan.work@outlook.com>
2024-04-15 17:31:26 +0100

    Update flight control algroithm and MahonyFilter

---
+++ SkyPulseUAV_FCS/DRIVER/flightcontrol.h
@@ -0,0 +1,31 @@
+#ifndef FLIGHTCONTROL_H
+#define FLIGHTCONTROL_H
+
+#include <QObject>
+#include <QVector>
+
+class FlightControl : public QObject
+{
+    Q_OBJECT
+public:
+    explicit FlightControl(int pitch, int yaw, int roll, int thrust, QObject *parent = nullptr);
+    ~FlightControl();
+
+    void initialize();
+    void setPitch(int pwmValue);
+    void setYaw(int pwmValue);
+    void setRoll(int pwmValue);
+    void setThrust(int pwmValue);
+
+    void updateMotorSpeeds(double pitch, double roll, double yaw, double thrust);
+
+private:
+    int Pitch_, Yaw_, Roll_, Thrust_;
+
+    quint8 clampToUint8(double value);
+
+signals:
+    void sig_sendMotorData(QVector<quint8> Motor_PWM);
+};
+
+#endif // FLIGHTCONTROL_H
